147aa05f22b558e3a76c498e4026c2503d5a10a4
werari <weronikaarim@yahoo.com>
2018-11-13 20:34:13 +0100

    initial commit

---
+++ src/main/java/servlets/CalculatorServlet.java
@@ -0,0 +1,74 @@
+package servlets;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CalculatorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Content-Type", "text/html");
+        Integer a = mapToInt(req.getParameter("a"));
+        Integer b = mapToInt(req.getParameter("b"));
+        String operation = req.getParameter("operation");
+        String message = req.getParameter("message");
+
+        PrintWriter writer = resp.getWriter();
+        createMessage(message, writer);
+        createForm(writer);
+        writer.print("Result: ");
+        if ("+".equals(operation)) {
+            writer.println(a + b);
+        } else if ("-".equals(operation)) {
+            writer.println(a - b);
+        } else if ("*".equals(operation)) {
+            writer.println(a * b);
+        } else if ("/".equals(operation)) {
+            if (b == 0) {
+                // resp.setStatus(302);
+                // resp.addHeader("Location", "https://pl.wikipedia.org/wiki/Dzielenie");
+                // String contextPath = req.getContextPath();
+//                System.out.println(req.getPathInfo());
+//                System.out.println(req.getContextPath());
+//                System.out.println(req.getServletPath());
+//                System.out.println(req.getPathTranslated());
+                resp.sendRedirect( req.getContextPath()+ req.getServletPath() + "?message=Nie dziel przez zero");
+
+            } else {
+                writer.println((double) a / b);
+            }
+        }
+    }
+
+    private void createMessage(String message, PrintWriter writer) {
+        if (message != null) {
+            writer.println("<p style= \"color: red;\">" + message + "</p>");
+        }
+    }
+
+    private void createForm(PrintWriter writer) {
+        writer.println("<form action=\"\">");
+        writer.println("<input type=\"number\" name=\"a\">");
+        writer.println("<select name=\"operation\">");
+        writer.println("<option value=\"+\">Dodawanie</option>");
+        writer.println("<option value=\"-\">Odejmowanie</option>");
+        writer.println("<option value=\"*\">Mnozenie</option>");
+        writer.println("<option value=\"/\">Dzielenie</option>");
+        writer.println("</select>");
+        writer.println("<input type=\"number\" name=\"b\">");
+        writer.println("<input type=\"submit\" value=\"=\">");
+        writer.println("</form>");
+    }
+
+    private Integer mapToInt(String value) {
+        if (value == null || !StringUtils.isNumeric(value)) {
+            return 0;
+        }
+        return Integer.valueOf(value);
+    }
+}
---
+++ src/main/java/servlets/HomeServlet.java
@@ -0,0 +1,17 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1>Hello world</h1>");
+        writer.println("<h1>Dupa 1234</h1>");
+    }
+}
